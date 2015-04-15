f3d1df9f6e8f76ca155cd7ad38051f48bede34a4
Chelsea <cldougl@gmail.com>
2015-04-15 16:08:52 -0400

    Update global_new.R

--- ggplot2/UN/global_new.R
+++ ggplot2/UN/global_new.R
@@ -1,10 +1,11 @@
-# Golbal environment for both ui and server
+# Golbal environment: code needed for both ui and server scripts
 library(dplyr)
-
 # Load data
-UN <- read.csv("Data/UN_IdealPoints.csv", stringsAsFactors=F)
-UN <- UN[complete.cases(UN),]
-
+UN <- read.csv("Data/IdealPoints_smallset.csv", stringsAsFactors=F)
+#UN <- UN[complete.cases(UN),]
+#^not necessary- all cases are complete in this ex.. 
+#can be added for more complex ex.
 Ideal_Point_Data <- UN
 
+#Source needed to create plotly graph-used in all "global" scripts
 source("plotlyGraphWidget.R")
