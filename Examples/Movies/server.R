541f579b1024b2b9c34b9617e2de3e9520d91bf9
Chelsea <cldougl@gmail.com>
2015-04-27 18:04:24 -0400

    update

--- Examples/Movies/server.R
+++ Examples/Movies/server.R
@@ -1,49 +1,37 @@
-## Make histogram of movie ratings
-
-## Add data
-# This example is using the movies data (built into ggplot)
+# server.R
 x = movies$rating
 minx = min(x)
 maxx = max(x)
 
 shinyServer(function(input, output) {
 			output$trendPlot <- renderGraph({
-									#pull number of bins from the ui
 									bins <- input$bins
-									
-									# draw the histogram with
-									# the specified number of bins		
-									trace1 <- list(
-														x = x, 
-														autobinx = FALSE, 
-														xbins = list(
-															start = minx, 
-															end = maxx, 
-															size =  ((maxx-minx)/bins)
-															),  
-														type = "histogram"
-														)
-		
-					# define data
+									trace1 <- list(x = x,
+																 autobinx = FALSE,
+																 xbins = list(start = minx,
+																							end = maxx,
+																							size =  ((maxx-minx)/bins)
+																 							),
+																 type = "histogram"
+																 )
+
 					data <- list(trace1)
-					# define layout information
-					layout<- list(xaxis = 
+					layout<- list(xaxis =
 												list(title = "Ratings",
-														 range = c(minx, maxx), 
-														 autorange=FALSE, 
-														 autotick=FALSE, 
-														 tick0=minx, 
-														 dtick= ((maxx-minx)/bins )))
-		
-		# This sends message up to the browser client, which will get fed through to
-		# Plotly's javascript graphing library embedded inside the graph
+														 range = c(minx, maxx),
+														 autorange = FALSE,
+														 autotick = FALSE,
+														 tick0 = minx,
+														 dtick = ((maxx-minx)/bins)
+												)
+										)
+
 		return(list(
-			list(
-				id="trendPlot",
-				task="newPlot",
-				data=data,
-				layout=layout
-			)
+					 list(id = "trendPlot",
+								task = "newPlot",
+								data = data,
+								layout = layout
+				)
 		))
 	})
 })
